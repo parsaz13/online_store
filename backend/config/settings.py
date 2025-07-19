40cf4e358871732a6fdf6dc2a4a08c2399a08c6d
parsa zarchini <zarchiniparsa68@gmail.com>
2025-07-19 12:20:59 +0330

    Update database password in settings and add comment in CustomUserManager for clarity

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'onlinestore',
         'USER': 'postgres',
-        'PASSWORD': '11012',
+        'PASSWORD': '1234papa',
         'HOST': 'localhost',
         'PORT': '5432',
     }
